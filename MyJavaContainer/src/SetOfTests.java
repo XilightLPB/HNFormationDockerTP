9ee8f577301e990cce97886206db2e5c64955314
XilightLPB <erwan.maigne-montamat@epita.fr>
2023-12-22 15:53:08 +0100

    this should work but I couldn't test the angular tho

---
+++ MyJavaContainer/src/SetOfTests.java
@@ -0,0 +1,18 @@
+public class SetOfTests {
+    public static void main(String[] args) {
+        System.out.println("Let's run our tests");
+        test1();
+        test2();
+        System.out.println("all our tests runned successfully");
+    }
+
+    public static void test1(){
+
+        System.out.println("first test runned smoothly");
+    }
+
+    public static void test2(){
+
+        System.out.println("Second test runned smoothly");
+    }
+}
